27dcff7109364c1132fc45518b3f7a86817671d9
Xartd0 <43171120+xartd0@users.noreply.github.com>
2022-07-26 02:08:04 +0300

    Update user_calls.py

--- tgbot/handlers/user_calls.py
+++ tgbot/handlers/user_calls.py
@@ -41,10 +41,16 @@ async def end_of_upload(message: types.Message, state: FSMContext):
 async def catalog(call: types.CallbackQuery):
     await call.message.delete()
     wallpaper = db.take_wallpaper()[0]
-    await call.message.answer_photo(
-        wallpaper[1],
-        caption=f'<b>Айди - <i>{wallpaper[0]}</i>\nАвтор - <i>{wallpaper[4]}</i>\nДобавлено - <i>{wallpaper[3]}</i>\nЛайков - <i>{wallpaper[5]}</i></b>'
-        ,reply_markup=catalog_buttons)
+    try:
+        await call.message.answer_photo(
+            wallpaper[1],
+            caption=f'<b>Айди - <i>{wallpaper[0]}</i>\nАвтор - <i>{wallpaper[4]}</i>\nДобавлено - <i>{wallpaper[3]}</i>\nЛайков - <i>{wallpaper[5]}</i></b>'
+            ,reply_markup=catalog_buttons)
+    except:
+        await call.message.answer_animation(
+            wallpaper[1],
+            caption=f'<b>Айди - <i>{wallpaper[0]}</i>\nАвтор - <i>{wallpaper[4]}</i>\nДобавлено - <i>{wallpaper[3]}</i>\nЛайков - <i>{wallpaper[5]}</i></b>'
+            ,reply_markup=catalog_buttons)      
     
 async def like(call: types.CallbackQuery):
     id = (call.message.caption).split(' - ')[1][0]
@@ -54,10 +60,16 @@ async def like(call: types.CallbackQuery):
         db.add_user_likes(user_id, id)
         wallpaper = db.take_wallpaper_by_id(id)[0]
         await call.message.delete()
-        await call.message.answer_photo(
-            wallpaper[1],
-            caption=f'<b>Айди - <i>{wallpaper[0]}</i>\nАвтор - <i>{wallpaper[4]}</i>\nДобавлено - <i>{wallpaper[3]}</i>\nЛайков - <i>{wallpaper[5]}</i></b>'
-            ,reply_markup=catalog_buttons)
+        try:
+            await call.message.answer_photo(
+                wallpaper[1],
+                caption=f'<b>Айди - <i>{wallpaper[0]}</i>\nАвтор - <i>{wallpaper[4]}</i>\nДобавлено - <i>{wallpaper[3]}</i>\nЛайков - <i>{wallpaper[5]}</i></b>'
+                ,reply_markup=catalog_buttons)
+        except:
+            await call.message.answer_animation(
+                wallpaper[1],
+                caption=f'<b>Айди - <i>{wallpaper[0]}</i>\nАвтор - <i>{wallpaper[4]}</i>\nДобавлено - <i>{wallpaper[3]}</i>\nЛайков - <i>{wallpaper[5]}</i></b>'
+                ,reply_markup=catalog_buttons)
     else:
         await call.answer('Вы уже поставили лайк!')
 
@@ -65,6 +77,10 @@ async def back(call: types.CallbackQuery):
     await call.message.delete()
     await call.message.answer(
         'Меню.', reply_markup=main_menu)
+
+async def inf(call: types.CallbackQuery):
+    await call.message.edit_text('<b>Данный бот - это аналог wallpaper engine, но в телеграме и для телефона, буду его обновлять, добавляйте свои обои!</b>', reply_markup=main_menu_back)
+
 
 
 def register_calls(dp: Dispatcher):
@@ -80,6 +96,9 @@ def register_calls(dp: Dispatcher):
 
     dp.register_callback_query_handler(like, 
     lambda call: call.data == 'like_wallpaper', state='*')
+
+    dp.register_callback_query_handler(inf, 
+    lambda call: call.data == 'information', state='*')
 
     dp.register_message_handler(end_of_upload,
     content_types=['photo','animation'],
