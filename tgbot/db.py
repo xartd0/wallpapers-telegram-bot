ccbcba527d166eafcdc070834f8d03142c576d38
Xartd0 <43171120+xartd0@users.noreply.github.com>
2022-07-27 20:35:51 +0300

    Update db.py

--- tgbot/db.py
+++ tgbot/db.py
@@ -20,38 +20,49 @@ class Data:
         with self.connection:
             self.cursor.execute("INSERT INTO bot_wallpapers VALUES (null, ?, ?, ?, ?, ?)", (wallpaper))
             logger.info(f'Wallpaper {wallpaper} added')
-    
-    def take_wallpaper(self, anti_id):
-        with self.connection:
-            self.cursor.execute("select * from bot_wallpapers where id != ? ORDER by random() limit 1", (anti_id,))
-            return self.cursor.fetchall()
 
     def take_wallpaper_by_id(self, id):
         with self.connection:
             self.cursor.execute("SELECT * FROM bot_wallpapers WHERE id = ?", (id,))
             return self.cursor.fetchall()
 
-    def add_like(self, id):
+    def add_like(self, id, user_id):
         with self.connection:
             self.cursor.execute("UPDATE bot_wallpapers SET likes = likes + 1 WHERE id = ?", (id,))
+            self.cursor.execute("INSERT INTO user_likes VALUES (?, ?)", (user_id, id))
+
+    def remove_like(self, id, user_id):
+        with self.connection:
+            self.cursor.execute("UPDATE bot_wallpapers SET likes = likes - 1 WHERE id = ?", (id,))
+            self.cursor.execute("DELETE FROM user_likes WHERE tg_id = ? and wall_id = ?", (user_id, id))
 
     def add_anti_id(self, anti_id, user_id):
         with self.connection:
             self.cursor.execute("UPDATE bot_users SET anti_id = ? WHERE tg_id = ?", (anti_id,user_id,))
-
-    def add_user_likes(self, user_id, wall_id):
-        with self.connection:
-            self.cursor.execute("INSERT INTO user_likes VALUES (?, ?)", (user_id, wall_id))
 
     def check_user(self, user_id):
         with self.connection:
             self.cursor.execute("SELECT name FROM bot_users WHERE tg_id = ?", (user_id,))
             return self.cursor.fetchone()
 
-    def check_anti_id(self, user_id):
+    def get_all_users(self):
         with self.connection:
-            self.cursor.execute("SELECT anti_id FROM bot_users WHERE tg_id = ?", (user_id,))
+            self.cursor.execute("SELECT count(*) FROM bot_users")
             return self.cursor.fetchone()[0]
+
+    def get_all_ids(self):
+        with self.connection:
+            clear_ids = []
+            for row in self.cursor.execute("SELECT id FROM bot_wallpapers"):
+                clear_ids.append(row[0])
+            return clear_ids
+
+    def get_all_users_ids(self):
+        with self.connection:
+            clear_ids = []
+            for row in self.cursor.execute("SELECT tg_id FROM bot_users"):
+                clear_ids.append(row[0])
+            return clear_ids
 
     def check_like(self, user_id, wall_id):
         with self.connection:
