244cf48b6de8777c6105c7045c0c7a281fa03c7a
Xartd0 <43171120+xartd0@users.noreply.github.com>
2022-08-01 02:02:02 +0300

    Update db.py

--- tgbot/db.py
+++ tgbot/db.py
@@ -8,12 +8,12 @@ class Data:
         
     def create_user(self, user):
         with self.connection:
-            self.cursor.execute("INSERT INTO bot_users VALUES (null, ?, ?, ?, ?, ?, ?)", (user))
+            self.cursor.execute("INSERT INTO bot_users VALUES (null, ?, ?, ?, ?, ?, ?, ?)", (user))
             logger.info(f'User {user} added')
 
     def add_wallpaper(self, wallpaper):
         with self.connection:
-            self.cursor.execute("INSERT INTO bot_wallpapers VALUES (null, ?, ?, ?, ?, ?)", (wallpaper))
+            self.cursor.execute("INSERT INTO bot_wallpapers VALUES (null, ?, ?, ?, ?, ?, ?)", (wallpaper))
             logger.info(f'Wallpaper {wallpaper} added')
 
     def take_wallpaper_by_id(self, id):
@@ -45,6 +45,11 @@ class Data:
             self.cursor.execute("SELECT count(*) FROM bot_users")
             return self.cursor.fetchone()[0]
 
+    def get_cat_by_id(self, id):
+        with self.connection:
+            self.cursor.execute("SELECT name FROM categories WHERE id = ?", (id,))
+            return self.cursor.fetchone()[0]
+
     def get_user_likes(self, user_id):
         with self.connection:
             self.cursor.execute("SELECT sum(likes) FROM bot_wallpapers WHERE owner_id = ?", (user_id,))
@@ -64,6 +69,15 @@ class Data:
         with self.connection:
             self.cursor.execute("UPDATE bot_users SET notif = ? WHERE tg_id = ?", (notif,user_id,))
 
+    def add_filter(self, temp, user_id):
+        with self.connection:
+            self.cursor.execute("UPDATE bot_users SET temp = ? WHERE tg_id = ?", (str(temp),user_id,))
+
+    def get_user_filters(self, user_id):
+        with self.connection:
+            self.cursor.execute("SELECT temp FROM bot_users WHERE tg_id = ?", (user_id,))
+            return self.cursor.fetchone()[0]
+
     def get_user_wallpapers(self, user_id):
         with self.connection:
             self.cursor.execute("SELECT count(*) FROM bot_wallpapers WHERE owner_id = ?", (user_id,))
@@ -73,10 +87,17 @@ class Data:
         with self.connection:
             self.cursor.execute("DELETE FROM bot_wallpapers WHERE id = ?", (wall_id,))
 
-    def get_all_ids(self):
+    def get_all_ids(self,ids):
         with self.connection:
             clear_ids = []
-            for row in self.cursor.execute("SELECT id from bot_wallpapers"):
+            for row in self.cursor.execute(f"SELECT id from bot_wallpapers WHERE cat in ({ids})"):
+                clear_ids.append(row[0])
+            return clear_ids
+
+    def get_all_ids_filter(self, id):
+        with self.connection:
+            clear_ids = []
+            for row in self.cursor.execute("SELECT id from bot_wallpapers WHERE cat = ?", (id,)):
                 clear_ids.append(row[0])
             return clear_ids
 
@@ -96,6 +117,12 @@ class Data:
         with self.connection:
             self.cursor.execute("select count(*) from bot_wallpapers")
             return self.cursor.fetchone()[0]
+
+    def get_all_cat(self):
+        with self.connection:
+            self.cursor.execute("select * from categories")
+            return self.cursor.fetchall()
+
 
     def get_top_users(self):
         with self.connection:
